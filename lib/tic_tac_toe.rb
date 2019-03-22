886ba230ad693f33e16e33d4c94912924487707f
Dennis Triplo <dennistriplo@yahoo.com>
2019-03-22 02:16:27 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,42 @@
+WIN_COMBINATIONS = [
+  [0,1,2], #top row
+  [3,4,5], #middle row
+  [6,7,8], #bottom row
+  [0,3,6], #first column
+  [1,4,7], #middle columns
+  [2,5,8], #last columns
+  [0,4,8], #right diagonal
+  [2,4,6]  #left diagonal
+]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, position, token)
+  board[position] = token
+end
+
+def position_taken?(board, position)
+  if board[position] != " "
+    return true
+  else
+    return false
+  end
+end
+
+def valid_move?(board, position)
+
+end
+
+
+
+end
